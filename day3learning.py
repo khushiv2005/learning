4b297b80d12a0a50cab631398c6e293d02897b49
khushiv2005 <khushiv9053@gmail.com>
2026-06-30 23:40:46 +0530

    some new work

---
+++ day3learning.py
@@ -0,0 +1,71 @@
+#wap to  find the year ,month,day ,week ,hours in given data
+#  
+  
+
+#for three years
+# no=int(input("enter the no of days:"))
+# year=no//365
+# remaining_day=no%365
+# month=remaining_day//30
+# week=remaining_day//7
+# day=remaining_day%7
+# hours=no%24
+# print("the number of years:",year)
+# print("the number of months:",month)
+# print("the number of days:",day)
+# print("the number of weeks:",week)
+# print("the number of hours:",hours) 
+
+
+#wap to creat a calculator 
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# print("the value of",a,"+",b,"is:",a+b)
+# print("the value of",a,"-",b,"is:",a-b)
+# print("the value of",a,"*",b,"is:",a*b)
+# print("the value of",a,"/",b,"is:",a/b)
+
+
+
+# #wap to find the simple interest
+# p=int(input("enter the principle amount:"))
+# r=int(input("enter the rate of interest:"))
+# t=int(input("enter the time:"))
+# si=(p*r*t)/100
+# print("the simple interest is:",si)
+
+#data type 
+# age=20
+# name="khushi"
+# birthday=2005,9,28
+# qualification="b.tech"
+# maths_marks=45.6
+# print( type(age))
+# print( type(name))
+# print( type(birthday))
+# print( type(qualification))
+# print( type(maths_marks))
+
+
+
+#relational operator
+# a=int(input("enter the first number:"))
+# b=int(input("enter the second number:"))
+# print("a>b:",a>b)
+# print("a<b:",a<b)
+# print("a==b:",a==b)
+# print("a!=b:",a!=b)
+# print("a>=b:",a>=b)
+# print("a<=b:",a<=b)
+
+#assingment operator
+# num=10
+# num=num+2
+# #num+=2
+# num=num-3
+# #num-=3
+# num=num*2
+# #num*=2
+# num=num%5
+# #num%=5
+# print("num:",num)
