e1df72c45a6f479f26444391c90758b471097f09
khushiv2005 <khushiv9053@gmail.com>
2026-07-04 23:14:05 +0530

    today work

---
+++ day5learning.py
@@ -0,0 +1,166 @@
+# #if condition only
+# # age=int(input("enter your age"))
+# # if(age>=18):
+# #     print("you are eligible for voting")
+# # if(age<18):
+# #     print("you are not eligible for voting")
+# # print("now i am satisfied")
+
+
+# # #next example
+# # #wap to find odd or even
+# # num=int(input("enter your number"))
+# # if(num%2==0):
+# #     print("the number will be even")
+# # if(num%2!=0):
+# #     print("the number will be odd")
+# # print("now i am satisfied")
+
+# #now if else condition
+# # age=int(input("enter your age"))
+# # if(age>=18):
+# #     print("you are eligible for driving")
+# # else:
+# #     print("you are not eligible for driving")
+# # print("now i am satisfied with this program")
+
+# # #wap to find odd or even using if else
+# # num=int(input("enter your number"))
+# # if(num%2==0):
+# #     print("the number will be even")
+# # else:
+# #     print("the number will be odd")
+# # print("now i am satisfied")
+
+# # #wap to find the laegest number  btw two number usinng ifelse
+# # a=int(input("enter the first number"))
+# # b=int(input("enter the second number:"))
+# # if(a>b):
+# #     print("the first number is largest")
+# # else:
+# #     print("the second number is largest")
+
+# # wap to find the largest number among three numbers using if else
+# a=int(input("enter the first number"))
+# b=int(input("enter the second number"))
+# c=int(input("enter the third number"))
+# if(a>b ):
+#     print("the first number is largest")
+# else:
+#     if(b>c):
+#         print("the second number is largest")
+#     else:
+#         if(c>a):
+#             print("the third number is largest")
+
+# #wap to find salary 
+# salary=int(input("enterr your salary"))
+# if(salary>=2500):
+#     print(salary+salary*20/100)
+# else:
+#     print(salary+salary*10/100)
+
+
+# #find salary of hra,ta,da
+# salary=int(input("enter your salary"))
+# hra=int(input("enter your hra"))
+# ta=int(input("enter your ta"))
+# da=int(input("enter your da"))
+# if(salary>=35000):
+#     print((hra+ta)-da)
+# else:
+#     print(hra*40/100-ta*30/100)
+
+
+
+#multiple if else problems
+#wap to find the age 
+# age=int(input("enter your age"))
+# if(age>=90):
+#     print("very very old")
+# elif(age>=70):
+#     print("very old")
+# elif(age>=50):
+#     print("old")
+# elif(age>=30):
+#     print("young")
+# elif(age>=15):
+#     print("child")
+
+#wap to find the grade by using multiple if else
+# marks=int(input("enter your marks"))
+# if(marks>=90):
+#     print("grade A")
+# elif(marks>=70):
+#     print("grade B")
+# elif(marks>=50):
+#     print("grade C")
+# elif(marks>=30):
+#     print("grade D")
+# else:
+#     print("fail")
+
+
+# # find evening ,morning and night
+# hour=int(input("enter your hour(0_23)"))
+# if(hour<12):
+#     print("morning")
+# elif(hour==12):
+#     print("noon")
+# elif(hour<=18):
+#     print("evening")
+#else:
+#print("night")
+
+
+#wap to find you have voting if yes or no then you are aligible for voting
+# age=int(input("enter your age"))
+# has_id=input("enter you have id?(yes/ no)")
+# if(age<18):
+#     print("you are samll,you are not elligible for vote")
+# elif has_id !="yes":
+#     print("without if your not elligible for voting")
+# else:
+#     print("you can givve vote")
+
+
+# wap to find age and ask have ticket for movie
+# age=int(input("enter your age"))
+# if(age>=18):
+#     has_ticket=input("enter have ticket? (yes/no)")
+#     if ("ticket==yes"):
+#         print("you can watch movie")
+#     else:
+#         print("phle ticket banwao")
+# else:
+#     print("you age is minor")
+
+
+#wap check username and password
+# username=input("enter your username")
+# if("username==admin"):
+#     password=input("enter your password")
+#     if ("password==1234"):
+#         print("login successfully")
+#     else:
+#         print("wrong password")
+# else:
+#     print("wrong username")
+
+#wap to check bill is more than thousand and its is gold digger pr not 
+bill= int(input("enter your bill"))
+if(bill>1000):
+    gold_member =input("enter are you gold member?(yes/no)")
+    if ("gold_member=yes"):
+        print("you got 20 percent discount")
+    else:
+        print("you got only 10percent discount")
+else:
+    print("discount k liye bill jayda hona chahiye")
+
+
+    #
+
+   
+
+
