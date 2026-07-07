3dcdd8812f3077a72041baf7a88c3c3af3cb3ae5
khushiv2005 <khushiv9053@gmail.com>
2026-07-07 22:03:54 +0530

    today work

---
+++ day6learning.py
@@ -0,0 +1,68 @@
+# simple for loop programm
+# for i in[10,20,15,2,5]:
+#     print(i)
+
+# use range
+# for i in range(1,11,2):
+    # print(i)
+
+
+# wap to write table 
+# no=int(input("enter your no"))
+# for i in range(no,no*11,no):
+#     print(i)
+
+# #wap to write table with proper format
+# no=int(input("enter your no"))
+# for i in range(1,11):
+#     c=i*no
+#     print(no,"x",i,"=",c)
+
+#wap again print the table
+# num=int(input("enter you num"))
+# for i in range(1,11):
+#     c=i*num
+#     print(num,"x",i,"=",c)
+
+#use while loop
+
+#wap to write a counting using while loop
+# a=1
+# while(a<=10):
+#     print(a)
+#     a=a+1
+
+# #wap to write a table
+# num=int(input("enter your num"))
+# i=1
+# while(i<=10):
+#     print(num*i)
+#     i=i+1
+
+#wap to write with table format
+# no=int(input("enter your no"))
+# a=1
+# while(a<=10):
+#     print(no,"x",a,"=",no*a)
+#     a=a+1
+
+#wap to print so manny table using for loop
+# no=int(input("enter your no"))
+# for i in range(1,no+1):# ye decide krta h table kha tak chlani h
+#     print()
+#     for j in range (1,11):# ye decide krta h ki 1 se 10 no tk chlani h
+#         z=i*j # i or j ko multiply krwa rha h
+#         print(z, end=" ")
+
+# #wap to print so many table using while loop 
+# no=int(input("enter your no"))
+# a=1
+# while(a<=no):
+#     print()
+#     b=1
+#     while(b<=10):
+#         c=a*b
+#         print(c,end=" ")
+#         b=b+1
+#     a=a+1
+        
